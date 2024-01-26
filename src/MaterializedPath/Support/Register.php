c3764ada2f463f426e8466c515d965a4ef8f764a
Andrej Rypo <xrypak@gmail.com>
2024-01-26 21:26:32 +0100

    register tweaks
    omits unused method and documents a caveat/edge-case

--- src/MaterializedPath/Support/Register.php
+++ src/MaterializedPath/Support/Register.php
@@ -13,10 +13,10 @@ final class Register
 {
     private array $nodes = [];
 
-    public function contains(array $vector): bool
-    {
-        return isset($this->nodes[$this->makeIndex($vector)]);
-    }
+//    public function contains(array $vector): bool
+//    {
+//        return isset($this->nodes[$this->makeIndex($vector)]);
+//    }
 
     public function push(array $vector, ShadowNode $node): void
     {
@@ -28,6 +28,19 @@ final class Register
         return $this->nodes[$this->makeIndex($vector)] ?? null;
     }
 
+    /**
+     * "Serializes" a path into a string usable as an index within a native PHP array.
+     *
+     * Note that if the tree paths relied upon `chr(7)` (ASCII #7) unprintable character
+     * (which is very unlikely), then this indexing might cause the tree building to fail for certain cases.
+     *
+     * To demonstrate using an example, imagine that instead of `chr(7)` and `chr(22)` a dot `.` delimiter and semicolon `:`
+     * were used in this method and the vectors contained only dots,
+     * then index produced by this method `2:....` would be the same for vectors `['..', '.']` and `['.', '..']`,
+     * the vectors would be indistinguishable and would cause the building algorithm to fail.
+     *
+     * This case is very unlikely, however, if not academic only.
+     */
     private function makeIndex(array $vector): string
     {
         return count($vector) . chr(22) . implode(chr(7), $vector);
